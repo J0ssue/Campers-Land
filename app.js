1c716d1a2b09d00ac83cec322b9c6872762eacaa
J0ssue <jdiaz66@icloud.com>
2018-12-30 19:32:46 +0000

    refactored campground schema into its own campground module inside of modules dir into campground.js file

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const express 	 = require('express'),
-			app 			 = express(),
-			bodyParser = require('body-parser'),
-			PORT 		 	 = 3000,
-			mongoose 	 = require('mongoose');
+const bodyParser = require('body-parser'),
+  Campground = require('./models/campground'),
+  mongoose = require('mongoose'),
+  express = require('express'),
+  app = express(),
+  PORT = 3000;
 
 // mongoose.connect creates the new collection inside data dir:
-mongoose.connect('mongodb://localhost:27017/campers_land', { useNewUrlParser: true });
+mongoose.connect(
+  'mongodb://localhost:27017/campers_land',
+  { useNewUrlParser: true }
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-// SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-	name: String,//expects name string
-	image: String,//expects image string
-	description: String, // ''
-});
-
-// starts the Campground schema:
-const Campground = mongoose.model('Campground', campgroundSchema);
 
 // creates new campgrounds with Campground class, takes in two parameters one for the new campground and another one is a callback for err handling and success handling:
 // Campground.create(
@@ -38,58 +32,58 @@ const Campground = mongoose.model('Campground', campgroundSchema);
 
 // Route to Home Page:
 app.get('/', (req, res) => {
-	res.render('landing');
+  res.render('landing');
 });
 
 // INDEX: Ahow all Campgrounds.
 app.get('/campgrounds', (req, res) => {
-	// Get all campgrounds from DB
-	Campground.find({}, (err, allCampgrounds) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.render('index', { campgrounds: allCampgrounds });
-		}
-	});
-	// res.render('campgrounds', { campgrounds });
+  // Get all campgrounds from DB
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { campgrounds: allCampgrounds });
+    }
+  });
+  // res.render('campgrounds', { campgrounds });
 });
 
 // CREATE: Add new campground to DB.
 app.post('/campgrounds', (req, res) => {
-	// get data from form and add to campgrounds array
-	let name 				= req.body.name,
-			image 			= req.body.image,
-			description = req.body.description;
-	const newCampground = { name, image, description };
-	// Create a new campground and save to DB:
-	Campground.create(newCampground, (err, newlyCreated) => {
-		if (err) {
-			console.log(err);
-		} else {
-			// redirect to campgrounds page:
-			res.redirect('/campgrounds');
-		}
-	});
+  // get data from form and add to campgrounds array
+  let name = req.body.name,
+    image = req.body.image,
+    description = req.body.description;
+  const newCampground = { name, image, description };
+  // Create a new campground and save to DB:
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect to campgrounds page:
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
 // NEW: show form to create new campground.
 app.get('/campgrounds/new', (req, res) => {
-	res.render('new.ejs');
+  res.render('new.ejs');
 });
 
 // SHOW: shows info about one campground;
 app.get('/campgrounds/:id', (req, res) => {
-	// find the campground with provided ID
-	Campground.findById(req.params.id, (err, foundCampground) => {
-		if (err) {
-			console.log(err);
-		} else {
-			// render show template with that campground
-			res.render('show', { campground: foundCampground });
-		}
-	});
+  // find the campground with provided ID
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render show template with that campground
+      res.render('show', { campground: foundCampground });
+    }
+  });
 });
 
 app.listen(PORT, () => {
-	console.log('App Running in localhost:3000');
+  console.log('App Running in localhost:3000');
 });
