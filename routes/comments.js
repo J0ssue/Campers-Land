fac0500f2549c03e34aae3cc6b1094859a2bd743
J0ssue <jdiaz66@icloud.com>
2019-01-10 11:58:13 +0000

    added delete route and logic to delete comments and added form with delete button for comments deletion

--- routes/comments.js
+++ routes/comments.js
@@ -72,6 +72,16 @@ router.put('/:comment_id', (req, res) => {
 });
 
 // DESTROY:
+router.delete('/:comment_id', (req, res) => {
+  console.log(req.params);
+  Comment.findByIdAndRemove(req.params.comment_id, err => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
 
 // AUTHENTICATION MIDDLEWARE:
 function isLoggedIn(req, res, next) {
