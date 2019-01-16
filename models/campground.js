8e479933aba9eb36f58ca0d9561b912786f96e1a
J0ssue <jdiaz66@icloud.com>
2019-01-16 09:51:09 +0000

    commented landing.css, added DYNAMIC PRICE functionality

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // CAMPGROUND SCHEMA CONFIG:
 const campgroundSchema = new mongoose.Schema({
   name: String,
+  price: String,
   image: String,
   description: String,
   author: {
